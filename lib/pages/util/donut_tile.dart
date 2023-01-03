37744f83651b0837aec5c6b3d92bff062f1e1ba6
Danielle Kom <97759611+daniellekom@users.noreply.github.com>
2023-01-03 12:12:55 -0500

    completed ui for donut

--- lib/pages/util/donut_tile.dart
+++ lib/pages/util/donut_tile.dart
@@ -34,18 +34,51 @@ class DonutTile extends StatelessWidget {
                 Container(
                   decoration: BoxDecoration(
                     color: donutColor[100],
-                    borderRadius: BorderRadius.circular(12),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(borderRadius),topRight:Radius.circular(borderRadius) ),
                   ),
                     padding: EdgeInsets.all(12),
-                    child: Text(donutPrice,style: TextStyle(color: donutColor,fontWeight: FontWeight.bold),)
+                    child: Text(
+                      '\$' + donutPrice,
+                      style: TextStyle(
+                          color: donutColor[800],
+                          fontWeight: FontWeight.bold,fontSize: 12),
+                    ),
                 ),
               ],
             ),
 
             // donut picture
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36.0,vertical: 16),
+              child: Image.asset(imageName),
+            ),
+
             // donut flavor
+            Text(donutFlavor,style: const TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+
+            const SizedBox(height: 4,),
+
+               Text('Dunkins',style: TextStyle(color: Colors.grey[500]),
+               ),
 
             // love icon + add button
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //love icon
+                  Icon(Icons.favorite,color: Colors.pink[400],
+                  ),
+                  //plus button
+                  Icon(Icons.add,color: Colors.grey[800]
+                  ),
+                    ],
+              ),
+            )
+
+
           ],
         ),
       ),
--- lib/tab/donut_tab.dart
+++ lib/tab/donut_tab.dart
@@ -17,7 +17,8 @@ class DonutTab extends StatelessWidget {
     return GridView.builder(
       itemCount: donutsOnSale.length,
       padding: EdgeInsets.all(12),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,childAspectRatio: 1/1.5),
       itemBuilder: (context, index) {
         return DonutTile(
           donutFlavor: donutsOnSale[index][0],
